fef46ab95a3ff222a538a2b533e06ac7958e3d54
hanmeng <hanm@buaa.edu.cn>
2022-04-27 00:45:31 +0800

    dramsim3 cosim

--- src/DramSimulator_DramSimulator.cpp
+++ src/DramSimulator_DramSimulator.cpp
@@ -48,63 +48,70 @@ JNIEXPORT jlong JNICALL Java_DramSimulator_DramSimulator_get_1clock_1ticks
 }
 
 jobject constructResponse(JNIEnv *env, jobject obj, CoDRAMResponse* response){
+    jboolean req_valid,resp_valid,is_write;
+    jlong address, req_time, resp_time, finish_time;
+    jint len,size,offset,id;
+    jbyte data[64];
+    jbyteArray metaData = env->NewByteArray(64);
 
+    if(response == NULL){
+        req_valid = false;
+        resp_valid = false;
+        is_write = false;
+        address = 0;
+        req_time = 0;
+        resp_time = 0;
+        finish_time = 0;
+        len = 0 ;
+        size = 0 ;
+        offset = 0 ;
+        id = 0 ;
+        memset(data,0,64);
+    } else{
+        resp_valid = true;
+        req_time = response->req_time;
+        resp_time = response->resp_time;
+        finish_time = response->finish_time;
+        const CoDRAMRequest* request = response->req;
+        if(request == NULL){
+            req_valid = false;
+            address = 0;
+            is_write = 0;
+            len = 0;
+            size = 0;
+            offset = 0;
+            id = 0;
+            memset(data, 0, 64);
+        }else{
+            req_valid = true;
+            address = request->address;
+            is_write = request->is_write;
+            struct dramsim3_meta* meta = (struct dramsim3_meta*) request->meta;
+            if(meta == NULL){
+                len = 0;
+                size = 0;
+                offset = 0;
+                id = 0;
+                memset(data, 0, 64);
+            }else{
+                len = meta->len;
+                size = meta->size;
+                offset = meta->offset;
+                id = meta->id;
+                memcpy(data,meta->data,64);
+            }
+        }
+    }
+    env->SetByteArrayRegion(metaData,0,64,data);
+    jclass thisClass = env->GetObjectClass(obj);
+    jmethodID ConstructResponseId = env->GetMethodID(thisClass,"constructResponse","(ZZJZIIII[BJJJ)LDramSimulator/DramResponse;");
+    jobject responseObj = env->CallObjectMethod(obj,ConstructResponseId,
+                                                req_valid,resp_valid,
+                                                address, is_write,
+                                                len,size,offset,id,metaData,
+                                                req_time,resp_time,finish_time);
 
-
-
-
-
-//    jclass responseClass = env->FindClass("LDramSimulator/Response");
-//    jmethodID RespConstructMethodId = env->GetMethodID(responseClass,"<init>","(ZLDramSimulator/Request;JJJ)V");
-//    printf("get ResponseClass and MethodId\n");
-//    jclass requestClass = env->FindClass("LDramSimulator/Request");
-//    jmethodID ReqConstructMethodId = env->GetMethodID(responseClass,"<init>","(ZJZIIII[B)V");
-//    printf("get RequestClass and MethodId\n");
-
-//    if(response != NULL){
-//        jlong req_time = response->req_time;
-//        jlong resp_time = response->resp_time;
-//        jlong finish_time = response->finish_time;
-//        jboolean valid = true;
-//
-//        jboolean req_valid = true;
-//        jlong req_address = response->req->address;
-//        jboolean req_is_write = response->req->is_write;
-//
-//        struct dramsim3_meta* meta = (struct dramsim3_meta*) response->req->meta;
-//
-//        jint meta_len = 0;
-//        jint meta_size = 0;
-//        jint meta_offset = 0;
-//        jint meta_id = 0;
-//        jbyteArray meta_data = env->NewByteArray(64);
-//
-//        if(meta != NULL){
-//            meta_len = meta->len;
-//            meta_size = meta->size;
-//            meta_offset = meta->offset;
-//            meta_id = meta->id;
-//
-//            uint64_t* data = meta->data;
-//            jbyte data_byte[64];
-//            memcpy(data_byte,data,64);
-//            env->SetByteArrayRegion(meta_data,0,64,data_byte);
-//        }
-//        jobject reqObj = env->NewObject(requestClass, ReqConstructMethodId, req_valid,req_address,req_is_write,
-//                                        meta_len,meta_size,meta_offset,meta_id,meta_data);
-//        printf("create reqObj\n");
-//        jobject respObj = env->NewObject(responseClass,RespConstructMethodId,valid, reqObj, req_time, resp_time, finish_time);
-//        printf("create respObj\n");
-//        return  respObj;
-//    } else{
-//        jobject reqObj = env->NewObject(requestClass, ReqConstructMethodId, false, 0, false,
-//                                        0,0,0,0,NULL);
-//        printf("create reqObj\n");
-//
-//        jobject respObj = env->NewObject(responseClass,RespConstructMethodId,false, reqObj, 0, 0, 0);
-//        printf("create respObj\n");
-//        return respObj;
-//    }
+    return responseObj;
 }
 JNIEXPORT jobject JNICALL Java_DramSimulator_DramSimulator_check_1write_1response
         (JNIEnv *env, jobject obj){
